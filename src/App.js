54cb3266c5ecf59292433f7a20056521f93859a9
Taher <taher.benismail@gmail.com>
2023-02-03 09:54:25 +0100

    taher commit

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>Hello wOrld</p>
+        <h1>Taher Commit</h1>
       </header>
     </div>
   );
